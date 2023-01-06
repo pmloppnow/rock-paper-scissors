80f41acc8c05b183de655a3e49629b46dfd656f9
Payton Loppnow <99100640+pmloppnow@users.noreply.github.com>
2023-01-06 15:22:19 -0500

    Add files via upload

---
+++ rock_paper_scissors.py
@@ -0,0 +1,41 @@
+# Allows 2 players to play rock, paper, scissors as many times as they wish. It tracks each player's wins and the 
+# number of draws.
+def determineWinner(player1, player2):
+    global draws
+    global player1_wins
+    global player2_wins
+    if player1 == player2:
+        draws += 1
+        return print(f"It's a draw. Player 1 has {player1_wins} wins, Player 2 has {player2_wins} wins, and there are {draws} draws.")
+    elif player1 == "r" and player2 == "s":
+        player1_wins += 1
+        return print(f"Player 1 wins. Player 1 has {player1_wins} wins, Player 2 has {player2_wins} wins , and there are {draws} draws.")
+    elif player1 == "s" and player2 == "p":
+        player1_wins += 1
+        return print(f"Player 1 wins. Player 1 has {player1_wins} wins, Player 2 has {player2_wins} wins, and there are {draws} draws.")
+    elif player1 == "p" and player2 == "r":
+        player1_wins += 1
+        return print(f"Player 1 wins. Player 1 has {player1_wins} wins, Player 2 has {player2_wins} wins, and there are {draws} draws.")
+    elif player1 == "s" and player2 == "r":
+        player2_wins += 1
+        return print(f"Player 2 wins. Player 1 has {player1_wins} wins, Player 2 has {player2_wins} wins, and there are {draws} draws.")
+    elif player1 == "p" and player2 == "s":
+        player2_wins += 1
+        return print(f"Player 2 wins. Player 1 has {player1_wins} wins, Player 2 has {player2_wins} wins, and there are {draws} draws.")
+    elif player1 == "r" and player2 == "p":
+        player2_wins += 1
+        return print(f"Player 2 wins. Player 1 has {player1_wins} wins, Player 2 has {player2_wins} wins, and there are {draws} draws.")
+    
+play_again = 'y'
+draws = 0
+player1_wins = 0
+player2_wins = 0
+
+while play_again == 'y':
+    player1 = input("Player 1: Type 'r' for rock, 'p' for paper, or 's' for scissors: ")
+    if player1 not in ('r', 'p', 's'): player1 = input("Player 1: Type 'r' for rock, 'p' for paper, or 's' for scissors: ")
+    player2 = input("Player 2: Type 'r' for rock, 'p' for paper, or 's' for scissors: ")
+    if player2 not in ('r', 'p', 's'): player2 = input("Player 2: Type 'r' for rock, 'p' for paper, or 's' for scissors: ")
+    determineWinner(player1, player2)
+    play_again = input("Would you like to play again? (y/n): ")
+    if play_again not in ('y', 'n'): play_again = input("Would you like to play again? (y/n): ")
